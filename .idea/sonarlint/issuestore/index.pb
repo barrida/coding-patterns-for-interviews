628b7bef79a2a328f172df10117b60a04e94abcd
suleyman.yildirim <suleymanube@gmail.com>
2022-07-26 21:58:31 +0100

    Pattern 4 - Two Pointers : Square sorted list

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+J
+GrokkingCodingPatterns.iml,1/a/1aa708a105b27120035c4f1499f44a8b6a4dabb7
--- src/main/java/pattern4/two_pointers/TwoPointers.kt
+++ src/main/java/pattern4/two_pointers/TwoPointers.kt
@@ -1,6 +1,6 @@
 package pattern4.two_pointers
 
-object RemoveDuplicates {
+object TwoPointers {
     /**
      * Given an array of sorted numbers, remove all duplicate number instances from it in-place, such that each element appears only once.
      *
@@ -25,13 +25,40 @@ object RemoveDuplicates {
      * @param key
      * @return
      */
-	@JvmStatic
-	fun remove(arr: IntArray, key: Int): Int {
+    @JvmStatic
+    fun remove(arr: IntArray, key: Int): Int {
         var nextElement = 0 // index of the next element which is not 'key'
         for (i in arr.indices) {
             nextElement = getNextNonDuplicate(arr, nextElement, i, key)
         }
         return nextElement
+    }
+
+    /**
+     *  Given a sorted array, create a new array containing squares of all the numbers of the input array in the sorted order.
+     */
+    @JvmStatic
+    fun makeSquares(arr: IntArray): IntArray {
+
+        var negativeIndex = 0
+        var positiveIndex = arr.size - 1
+        var highestSquareIndex = arr.size - 1
+        val squares = IntArray(arr.size);
+
+        while (negativeIndex < positiveIndex) {
+
+            val leftSquare = arr[negativeIndex] * arr[negativeIndex]
+            val rightSquare = arr[positiveIndex] * arr[positiveIndex]
+
+            if (leftSquare > rightSquare) {
+                squares[highestSquareIndex--] = leftSquare;
+                negativeIndex++
+            } else {
+                squares[highestSquareIndex--] = rightSquare;
+                positiveIndex--
+            }
+        }
+        return squares;
     }
 
     private fun getNextNonDuplicate(arr: IntArray, nextNonDuplicate: Int, i: Int, key: Int): Int {
@@ -42,4 +69,6 @@ object RemoveDuplicates {
         }
         return nextNonDuplicate
     }
+
+
 }
