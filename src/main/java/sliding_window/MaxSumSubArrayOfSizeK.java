bc756f007cd540d1e4b4413a1f28758cfff579ab
Suleyman Yildirim <suleymanube@gmail.com>
2022-12-25 22:52:00 +0000

    [sliding window] MaxSumSubArrayOfSizeK sliding window

--- src/main/java/sliding_window/MaxSumSubArrayOfSizeK.java
+++ src/main/java/sliding_window/MaxSumSubArrayOfSizeK.java
@@ -1,8 +1,10 @@
 package sliding_window;
 
+import java.util.Arrays;
+
 /**
  * Given an array of positive numbers and a positive number ‘k,’ find the maximum sum of any contiguous subarray of size ‘k’.
- *
+ * <p>
  * Input: [2, 1, 5, 1, 3, 2], k=3
  * Output: 9
  * Explanation: Subarray with maximum sum is [5, 1, 3].
@@ -11,15 +13,31 @@ package sliding_window;
  */
 public class MaxSumSubArrayOfSizeK {
 
-	public static int maxSumSubArrayBruteForce(int[] arr, int k ){
+	public static int maxSumSubArrayBruteForce(int[] arr, int k) {
 		int maxSum = 0;
 		int windowSum;
 		for (int i = 0; i <= arr.length - k; i++) {
 			windowSum = 0;
 			for (int j = i; j < i + k; j++) {
-				windowSum +=arr[j];
+				windowSum += arr[j];
 			}
 			maxSum = Math.max(windowSum, maxSum);
+		}
+		return maxSum;
+	}
+
+	public static int maxSumSubArraySlidingWindow(int[] arr, int k) {
+		int subArrSum = 0;
+		int maxSum = 0;
+		int subArrStartIndex = 0;
+		for (int subArrEndIndex = 0; subArrEndIndex < arr.length; subArrEndIndex++) {
+			subArrSum += arr[subArrEndIndex];
+			//sliding window size
+			if (subArrEndIndex >= k - 1) {
+				maxSum = Math.max(subArrSum, maxSum);
+				subArrSum -= arr[subArrStartIndex];
+				subArrStartIndex++;
+			}
 		}
 		return maxSum;
 	}
--- src/test/java/sliding_window/MaxSumSubArrayOfSizeKTest.java
+++ src/test/java/sliding_window/MaxSumSubArrayOfSizeKTest.java
@@ -13,4 +13,9 @@ class MaxSumSubArrayOfSizeKTest {
 		assertEquals(9, MaxSumSubArrayOfSizeK.maxSumSubArrayBruteForce(new int[]{2, 1, 5, 1, 3, 2}, 3));
 		assertEquals(7, MaxSumSubArrayOfSizeK.maxSumSubArrayBruteForce(new int[]{2, 3, 4, 1, 5}, 2));
 	}
+
+	@Test void maxSumSubArraySlidingWindow() {
+		assertEquals(9, MaxSumSubArrayOfSizeK.maxSumSubArraySlidingWindow(new int[]{2, 1, 5, 1, 3, 2}, 3));
+
+	}
 }
